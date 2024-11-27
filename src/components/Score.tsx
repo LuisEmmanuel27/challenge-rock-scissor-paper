5356ad1fee5fabd46104f572cbf6b589b2eb74f9
luiseprogramer27 <luiseprogramer27@gmail.com>
2024-11-27 18:43:58 +0000

    Moves assets and updates components
    Moves bonus images from `public/` to `src/assets/`.
    
    Updates `AdvancedGame` and `NormalGame` components to use the new image paths.  Also updates the `Score` component with styling and basic state.  Lastly, updates the layout to include the header border and padding.

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,7 +1,12 @@
+import { useState } from "preact/hooks"
+
 const Score = () => {
+  const [score, setScore] = useState<Number>(0)
+
   return (
-    <div>
-      <span>score</span>
+    <div className="bg-white flex items-center justify-center flex-col gap-1 py-6 px-12 rounded-2xl">
+      <span className="text-Neutral-ScoreText tracking-widest">SCORE</span>
+      <span className="text-Neutral-DarkText text-5xl font-bold">{score}</span>
     </div>
   )
 }
